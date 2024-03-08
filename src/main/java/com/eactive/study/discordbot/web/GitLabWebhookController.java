d47cdb4081bb47c35fa9e76e951d560ff7b15d28
sunwook1996 <hanyang9984@naver.com>
2024-03-09 03:13:25 +0900

    서버확인용 로깅

--- src/main/java/com/eactive/study/discordbot/web/GitLabWebhookController.java
+++ src/main/java/com/eactive/study/discordbot/web/GitLabWebhookController.java
@@ -1,18 +1,27 @@
 package com.eactive.study.discordbot.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class GitLabWebhookController {
 
     @RequestMapping("/testCall")
     public ResponseEntity<Map<String, String[]>> testCall(HttpServletRequest request) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            log.info(objectMapper.writeValueAsString(request.getParameterMap()));
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);
+        }
         return ResponseEntity.ok(request.getParameterMap());
     }
 
