35126b1301d301d64c0df8cf3124b47ec0dfca83
sunwook <sunwook@eactive.co.kr>
2024-03-08 22:42:50 +0900

    서버테스트용 설정변경

---
+++ src/main/java/com/eactive/study/discordbot/web/GitLabWebhookController.java
@@ -0,0 +1,18 @@
+package com.eactive.study.discordbot.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class GitLabWebhookController {
+
+    @GetMapping("/testCall")
+    public ResponseEntity<Map<String, String[]>> testCall(HttpServletRequest request) {
+        return ResponseEntity.ok(request.getParameterMap());
+    }
+
+}
